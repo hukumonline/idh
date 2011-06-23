5fb6e11cc0f4daf391b9033216c886e2de1e0ed9
root <root@localhost.localdomain>
2011-06-23 12:50:33 +0700

    added groupName free for migration

--- application/services/customer/account.php
+++ application/services/customer/account.php
@@ -341,6 +341,10 @@ class Account
 		{
 			$groupName = "Admin En";
 		}
+		else if ($groupId == 25) 
+		{
+			$groupName = "Free";
+		}
 		else if ($groupId == 26) 
 		{
 			$groupName = "Individual";
